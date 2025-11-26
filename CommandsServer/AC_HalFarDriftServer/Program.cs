b663b0fea1c18c9a737da84521613bc89d698ca8
Andreas Grech <agnt666@gmail.com>
2025-11-26 15:00:13 +0100

    Initial commit

---
+++ CommandsServer/AC_HalFarDriftServer/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+
+namespace AC_HalFarDriftServer
+{
+  public class DriftServerEndpoint : WebSocketBehavior
+  {
+    protected override void OnMessage (MessageEventArgs e)
+    {
+      var dataString = e.Data;
+      var isBinary = e.IsBinary;
+      var isPing = e.IsPing;
+      var isText = e.IsText;
+      var rawData = e.RawData;
+      
+      Console.WriteLine($"Message received: Data = {dataString}, Data Length = {dataString.Length}, IsBinary = {isBinary}, IsPing = {isPing}, IsText = {isText}, RawData Length = {rawData.Length}");
+
+      Send ("Message received, thank you!");
+    }
+  }
+
+  public class Program
+  {
+    public static void Main (string[] args)
+    {
+      var serverProtocol = "ws";
+      var serverAddress = "127.0.0.1";
+      var serverEndpoint = "DriftServer";
+
+      var baseServerAddress = $"{serverProtocol}://{serverAddress}";
+      var fullServerAddress = $"{baseServerAddress}/{serverEndpoint}";
+      Console.WriteLine($"Starting WebSocket server at {fullServerAddress}");
+
+      var wssv = new WebSocketServer (baseServerAddress);
+      wssv.AddWebSocketService<DriftServerEndpoint> ($"/{serverEndpoint}");
+      wssv.Start ();
+      Console.ReadKey (true);
+      wssv.Stop ();
+    }
+  }
+}
