1a2da034c41f51c83b007e923b27670addcbc4c9
Andreas Grech <agnt666@gmail.com>
2025-11-26 17:31:17 +0100

    moving endpoint to dir

---
+++ CommandsServer/AC_HalFarDriftServer/EndPoints/DriftServerEndpoint.cs
@@ -0,0 +1,78 @@
+﻿using System.Text;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+using ErrorEventArgs = WebSocketSharp.ErrorEventArgs;
+
+namespace AC_HalFarDriftServer.EndPoints;
+
+public class DriftServerEndpoint : WebSocketBehavior
+{
+    public static string EndpointName => "DriftServer";
+
+    protected override void OnMessage (MessageEventArgs e)
+    {
+        var dataString = e.Data;
+        var isBinary = e.IsBinary;
+        var isPing = e.IsPing;
+        var isText = e.IsText;
+        var rawData = e.RawData;
+      
+        Console.WriteLine($"Message received: Data = {dataString}, Data Length = {dataString.Length}, IsBinary = {isBinary}, IsPing = {isPing}, IsText = {isText}, RawData Length = {rawData.Length}");
+
+        Send ("Message received, thank you!");
+    }
+
+    protected override void OnOpen()
+    {
+        Console.WriteLine("New client connected.");
+        Send("Welcome to the Drift Server! (from blocking call)");
+        SendAsync("another message (from async call)", b =>
+        {
+            Console.WriteLine($"Sent async message, success: {b}");
+        });
+      
+        var currentWebSocketContext = Context;
+        var currentQueryStringKeyValueCollection = currentWebSocketContext.QueryString;
+        var allKeys = currentQueryStringKeyValueCollection.AllKeys;
+        var queryStringBuilder = new StringBuilder( /*capacity*/);
+        foreach (var queryStringKey in allKeys)
+        {
+            var queryStringValue = currentQueryStringKeyValueCollection[queryStringKey];
+            queryStringBuilder.Append($"{queryStringKey}={queryStringValue}&");
+        }
+      
+        // build the querystring into one string
+        Console.WriteLine($"Client QueryString: {queryStringBuilder.ToString()}");
+      
+
+        var sessions = Sessions.Sessions.ToArray();
+        Console.WriteLine($"Current active sessions count: {sessions.Length}");
+        foreach (var webSocketSession in sessions)
+        {
+            var webSocketSessionContext = webSocketSession.Context;
+            var id = webSocketSession.ID;
+            var protocol = webSocketSession.Protocol;
+            var startTime = webSocketSession.StartTime;
+            var state = webSocketSession.State;
+        
+            Console.WriteLine($"Session Info: ID = {id}, Protocol = {protocol}, StartTime = {startTime}, State = {state}");
+        }
+    }
+
+    protected override void OnClose(CloseEventArgs e)
+    {
+        var code = e.Code;
+        var reason = e.Reason;
+        var wasClean = e.WasClean;
+      
+        Console.WriteLine($"OnClose called: Code = {code}, Reason = {reason}, WasClean = {wasClean}");
+    }
+
+    protected override void OnError(ErrorEventArgs e)
+    {
+        var exception = e.Exception;
+        var message = e.Message;
+      
+        Console.WriteLine($"OnError called: Message = {message}, Exception = {exception}");
+    }
+}
--- CommandsServer/AC_HalFarDriftServer/Program.cs
+++ CommandsServer/AC_HalFarDriftServer/Program.cs
@@ -1,88 +1,17 @@
 ﻿using System;
-using System.Text;
+using AC_HalFarDriftServer.EndPoints;
 using WebSocketSharp;
 using WebSocketSharp.Server;
-using ErrorEventArgs = WebSocketSharp.ErrorEventArgs;
 
 namespace AC_HalFarDriftServer
 {
-  public class DriftServerEndpoint : WebSocketBehavior
-  {
-    protected override void OnMessage (MessageEventArgs e)
-    {
-      var dataString = e.Data;
-      var isBinary = e.IsBinary;
-      var isPing = e.IsPing;
-      var isText = e.IsText;
-      var rawData = e.RawData;
-      
-      Console.WriteLine($"Message received: Data = {dataString}, Data Length = {dataString.Length}, IsBinary = {isBinary}, IsPing = {isPing}, IsText = {isText}, RawData Length = {rawData.Length}");
-
-      Send ("Message received, thank you!");
-    }
-
-    protected override void OnOpen()
-    {
-      Console.WriteLine("New client connected.");
-      Send("Welcome to the Drift Server! (from blocking call)");
-      SendAsync("another message (from async call)", b =>
-      {
-        Console.WriteLine($"Sent async message, success: {b}");
-      });
-      
-      var currentWebSocketContext = Context;
-      var currentQueryStringKeyValueCollection = currentWebSocketContext.QueryString;
-      var allKeys = currentQueryStringKeyValueCollection.AllKeys;
-      var queryStringBuilder = new StringBuilder( /*capacity*/);
-      foreach (var queryStringKey in allKeys)
-      {
-        var queryStringValue = currentQueryStringKeyValueCollection[queryStringKey];
-        queryStringBuilder.Append($"{queryStringKey}={queryStringValue}&");
-      }
-      
-      // build the querystring into one string
-      Console.WriteLine($"Client QueryString: {queryStringBuilder.ToString()}");
-      
-
-      var sessions = Sessions.Sessions.ToArray();
-      Console.WriteLine($"Current active sessions count: {sessions.Length}");
-      foreach (var webSocketSession in sessions)
-      {
-        var webSocketSessionContext = webSocketSession.Context;
-        var id = webSocketSession.ID;
-        var protocol = webSocketSession.Protocol;
-        var startTime = webSocketSession.StartTime;
-        var state = webSocketSession.State;
-        
-        Console.WriteLine($"Session Info: ID = {id}, Protocol = {protocol}, StartTime = {startTime}, State = {state}");
-      }
-    }
-
-    protected override void OnClose(CloseEventArgs e)
-    {
-      var code = e.Code;
-      var reason = e.Reason;
-      var wasClean = e.WasClean;
-      
-      Console.WriteLine($"OnClose called: Code = {code}, Reason = {reason}, WasClean = {wasClean}");
-    }
-
-    protected override void OnError(ErrorEventArgs e)
-    {
-      var exception = e.Exception;
-      var message = e.Message;
-      
-      Console.WriteLine($"OnError called: Message = {message}, Exception = {exception}");
-    }
-  }
-
   public class Program
   {
     public static void Main (string[] args)
     {
       var serverProtocol = "ws";
       var serverAddress = "127.0.0.1";
-      var serverEndpoint = "DriftServer";
+      var serverEndpoint = DriftServerEndpoint.EndpointName;
 
       var baseServerAddress = $"{serverProtocol}://{serverAddress}";
       var fullServerAddress = $"{baseServerAddress}/{serverEndpoint}";
